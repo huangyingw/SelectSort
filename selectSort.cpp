91ded4a079c50e1aa8f1146895bd5b42f0aaeeb7
huangyingw@linux_programming <huangyingw@gmail.com>
2010-11-05 16:42:32 +0800

    n

--- selectSort.cpp
+++ selectSort.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main() 
 {
-	SelectSort<int>* selectSort=new SelectSort<int>(8);
-	selectSort->data[0]=8;
-	selectSort->data[1]=2;
-	selectSort->data[2]=1;
-	selectSort->data[3]=3;
-	selectSort->data[4]=5;
-	selectSort->data[5]=4;
-	selectSort->data[6]=6;
-	selectSort->data[7]=7;
-	selectSort->Sort();
-	selectSort->Print();
-	return 0;
+  SelectSort<int>* selectSort=new SelectSort<int>(8);
+  selectSort->data[0]=8;
+  selectSort->data[1]=2;
+  selectSort->data[2]=1;
+  selectSort->data[3]=3;
+  selectSort->data[4]=5;
+  selectSort->data[5]=4;
+  selectSort->data[6]=6;
+  selectSort->data[7]=7;
+  selectSort->Sort();
+  selectSort->Print();
+  return 0;
 }
 
--- selectSort.h
+++ selectSort.h
@@ -2,28 +2,28 @@
 using namespace std;
 template <class Type> class SelectSort
 {
-	private:
-		int length;
-		ofstream fout; 
-	public:
-		Type* data;
-		SelectSort<Type>(int len);
-		~SelectSort<Type>();
-		void Initial();
-		void Sort();
-		void Print();
+  private:
+    int length;
+    ofstream fout; 
+  public:
+    Type* data;
+    SelectSort<Type>(int len);
+    ~SelectSort<Type>();
+    void Initial();
+    void Sort();
+    void Print();
 };
 
 template <class Type> SelectSort<Type>::SelectSort(int len)
 {
-	length=len;
-	data=new Type[length];
-	fout.open("output.txt");
+  length=len;
+  data=new Type[length];
+  fout.open("output.txt");
 }
 
 template <class Type> SelectSort<Type>::~SelectSort()
 {
-	fout.close();
+  fout.close();
 }
 
 template <class Type> void SelectSort<Type>::Initial()
@@ -33,32 +33,32 @@ template <class Type> void SelectSort<Type>::Initial()
 
 template <class Type> void SelectSort<Type>::Print()
 {
-	for(int i=0;i<length;i++)
-	{
-		cout<<data[i]<<",";
-	}
-	cout<<endl;
+  for(int i=0;i<length;i++)
+  {
+    cout<<data[i]<<",";
+  }
+  cout<<endl;
 }
 
 template <class Type> void SelectSort<Type>::Sort()
 {
-	Type temp;
-	for(int i=0;i<length;i++)
-	{
-		int k=i;
-		for(int j=i+1;j<length;j++)
-		{
-			if(data[j]<data[k])
-			{
-				k=j;
-			}
-		}
-		
-		if(k!=i)
-		{
-			temp=data[k];
-			data[k]=data[i];
-			data[i]=temp;
-		}
-	}
+  Type temp;
+  for(int i=0;i<length;i++)
+  {
+    int k=i;
+    for(int j=i+1;j<length;j++)
+    {
+      if(data[j]<data[k])
+      {
+        k=j;
+      }
+    }
+
+    if(k!=i)
+    {
+      temp=data[k];
+      data[k]=data[i];
+      data[i]=temp;
+    }
+  }
 }
